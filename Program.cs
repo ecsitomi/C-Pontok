e395fed2f21bbcc3e492a4d5f1004f346a3d8d04
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2023-10-05 20:55:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pontok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pont pont1 = new Pont();
+            Pont pont2 = new Pont(1, 2);
+            Pont pont3 = new Pont(3);
+
+            Console.WriteLine($"p1 {pont1}, Távolsag {pont1.Tavolsag()}");
+            Console.WriteLine($"p2 {pont2}, Távolsag {pont2.Tavolsag()}");
+            Console.WriteLine($"p3 {pont3}, Távolsag {pont3.Tavolsag()}");
+
+            Pont[] pontok = new Pont[10];
+            //listánál lemaradtam
+
+        }
+    }
+}
---
+++ pont.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pontok
+{
+    public class Pont
+    {
+        private int x;
+        private int y;
+        private static Random random = new Random();
+
+        public Pont()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        public Pont(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Pont(int n)
+        {
+            this.x = random.Next(-n, n + 1);
+            this.y = random.Next(-n, n + 1);
+        }
+
+        public int X
+        {
+            get => x;
+            set => x = value;
+        }
+
+        public int Y
+        {
+            get => y;
+            set => y = value;
+        }
+
+        public double Tavolsag()
+        {
+            return Math.Sqrt(this.x * this.x + this.y * this.y);
+        }
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+    }
+}
